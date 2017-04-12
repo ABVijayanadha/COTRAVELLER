9b9517633d0de47e5f95ded36856ec0e5c10aead
ABVijayanadha <arunbabu.kottu@hotmail.com>
2017-04-12 17:12:38 +0530

    Typo error in routes

--- config/routes.js
+++ config/routes.js
@@ -2,10 +2,9 @@ var userController = require('./../app/controller/users.js');
 var registration = require('./../app/controller/registration.js');
 
 module.exports = function(app, passport, auth) {
-	app.post('/regitration',registration.register);
+	app.post('/registration',registration.register);
 	app.post('/login',registration.login);
 	app.get('/logout',registration.logout);
 	// app.use('/api/user',userController);
 	app.use('/api/user',passport.authenticate('jwt', { session: false }),userController);
 }
-
